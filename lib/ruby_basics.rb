5beeffd257436d894944861ee01ecb83add0775b
Ivan Szebenszki <szib77@gmail.com>
2019-03-07 20:39:04 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -15,7 +15,7 @@ def greeting(greeting, name)
 end
 
 def return_a_value
-  "Nice"
+  return "Nice"
 end
 
 def last_evaluated_value
@@ -23,5 +23,5 @@ def last_evaluated_value
 end
 
 def pizza_party
-
+  "cheese"
 end
